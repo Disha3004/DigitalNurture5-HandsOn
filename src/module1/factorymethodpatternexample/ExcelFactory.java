bc02e52abf50f963ac3332c07bfe762526a3453c
Disha3004 <dishakamath3004@gmail.com>
2026-06-11 20:14:59 +0530

    Completed Singleton and Factory Method Pattern Exercises

---
+++ src/module1/factorymethodpatternexample/ExcelFactory.java
@@ -0,0 +1,9 @@
+package factorymethodpatternexample;
+
+public class ExcelFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
---
+++ src/module1/factorymethodpatternexample/WordFactory.java
@@ -0,0 +1,9 @@
+package factorymethodpatternexample;
+
+public class WordFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
