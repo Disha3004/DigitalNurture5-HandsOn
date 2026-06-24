4b6d05f9779cf9c4eea673474d07389be9d700f6
Disha3004 <dishakamath3004@gmail.com>
2026-06-24 11:44:17 +0530

    financial forecast

---
+++ src/module2/financialforecasting/FinancialForecast.java
@@ -0,0 +1,24 @@
+package src.module2.financialforecasting;
+
+public class FinancialForecast {
+
+    static double futureValue(double currentValue, double growthRate, int years) {
+
+        if (years == 0)
+            return currentValue;
+
+        return futureValue(currentValue, growthRate, years - 1)
+                * (1 + growthRate);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double result = futureValue(currentValue, growthRate, years);
+
+        System.out.println("Future Value in "+years+" year: " + result);
+    }
+}
