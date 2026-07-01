416b134cff0d6a9b4a552b33de0d05708fa602e5
Disha3004 <dishakamath3004@gmail.com>
2026-07-01 10:46:22 +0530

    junit

---
+++ src/tdd/tdd/src/test/java/com/example/junit/Calculatortest.java
@@ -0,0 +1,21 @@
+package com.example.junit;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Calculatortest {
+
+    @Test
+    void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(10, calculator.add(7, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.subtract(7, 3));
+    }
+}
